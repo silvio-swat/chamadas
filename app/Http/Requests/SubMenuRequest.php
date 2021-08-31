638b28d62133947edc3caa55fa9591f2dfbb8b7e
Silvio Watakabe <koomakai@gmail.com>
2021-08-31 10:14:49 -0500

    - Inserido toast - Fase de implementação da ACL

---
+++ app/Http/Requests/SubMenuRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SubMenuRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'subMenuModel.name'         => 'required|string|min:3',
+            'subMenuModel.controller'   => 'required|string|min:3',
+            'subMenuModel.action'       => '',            
+            'subMenuModel.menu_id'      => 'required',
+            'subMenuModel.order'        => 'required|integer|min:1',
+            'subMenuModel.icon'         => 'required',
+        ];
+    }
+
+     /**
+     * Custom message for validation
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'subMenuModel.name.required'        => 'Digite o nome do Item de menu',
+            'subMenuModel.controller.required'  => 'Digite o Controller ou component livewire',
+            'subMenuModel.order.required'       => 'Insira um número para ordenar',
+            'subMenuModel.icon.required'        => 'Selecione um ícone!',
+        ];
+    }     
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
@@ -19,10 +18,15 @@
         </style>          
 
         @livewireStyles  
+        {{-- @toastr_css --}}
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
 
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
+
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -51,8 +55,36 @@
 
         @livewireScripts
 
-        <script src="{{ asset('js/app.js') }}" defer></script>       
+        <script src="{{ asset('js/app.js') }}" defer></script>   
         
+        {{-- Comentado Toast pois não está sendo usando --}}
+        {{-- @jquery
+
+        <script type="text/javascript" src="{{asset('lightbox/js/lightbox.js')}}"></script>
         
+        @toastr_js
+        @toastr_render
+        
+        <script>
+            $("#toggle").click(function () {
+                $(this).toggleClass("on")
+                $("#resize").toggleClass("active")
+            })
+        </script>  --}}
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+        <script>
+            window.addEventListener('alert', event => { 
+                         toastr[event.detail.type](event.detail.message, 
+                         event.detail.title ?? ''), toastr.options = {
+                                "closeButton": true,
+                                "progressBar": true,
+                            }
+                        });
+            </script>        
+
     </body>
 </html>
